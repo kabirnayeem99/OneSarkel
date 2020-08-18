26d03c2529c05f7b7ef8a83454763a4280804b06
kabirnayeem99 <kabirnayeem.99@gmail.com>
2020-08-18 12:10:26 +0600

    📝 textfield done with a perfect send button

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -11,7 +11,7 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: SamsungColor.black,
@@ -31,7 +31,52 @@ class _ChatScreenState extends State<ChatScreen> {
         body: Container(
           color: SamsungColor.black,
           child: Column(
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ListView.builder(itemBuilder: null),
+              Container(
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          right: 2.0,
+                          left: 10.0,
+                          top: 10.0,
+                          bottom: 10.0,
+                        ),
+                        child: TextField(
+                          style: TextStyle(color: Colors.black),
+                          textAlignVertical: TextAlignVertical.top,
+                          textAlign: TextAlign.left,
+                          cursorColor: Colors.black,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(60.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            fillColor: Colors.white,
+                            filled: true,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: IconButton(
+                        iconSize: 40.0,
+                        icon: Icon(Icons.send),
+                        onPressed: () {
+                          print("send button tapped");
+                        },
+                        color: SamsungColor.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
