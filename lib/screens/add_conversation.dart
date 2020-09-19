b6d2037af63064b9219e3f7060399363e9a1fef9
Naimul Kabir <kabirnayeem.99@gmail.com>
2020-09-19 16:24:21 +0600

    🚸 IMPROVE: Improves search bar accessibility.
    Changes font size in search bar
    changes bg color to light grey
    changes page background to samsung black
    icon to primary samsung dark

--- lib/screens/add_conversation.dart
+++ lib/screens/add_conversation.dart
@@ -1,14 +1,28 @@
 import 'package:flappy_search_bar/flappy_search_bar.dart';
+import 'package:flappy_search_bar/search_bar_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_samsung_messaging_app_clone/theme/samsung_color.dart';
 
 class AddConversation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: SamsungColor.black,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: SearchBar(),
+          child: SearchBar(
+            iconActiveColor: SamsungColor.primary,
+            textStyle: TextStyle(
+              color: SamsungColor.white,
+              fontSize: 20.0,
+            ),
+            searchBarStyle: SearchBarStyle(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              backgroundColor: SamsungColor.lightGrey,
+              borderRadius: BorderRadius.circular(40.0),
+            ),
+          ),
         ),
       ),
     );
